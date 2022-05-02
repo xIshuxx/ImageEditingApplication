3d7d18cf46d44bfa542290ea9acb762439c11da3
Ishaan <ishaanvash@gmail.com>
2022-05-02 23:00:14 +0700

    Image Store in ImageView.

--- app/src/main/java/com/example/imgeditor/ImgEditing.kt
+++ app/src/main/java/com/example/imgeditor/ImgEditing.kt
@@ -1,16 +1,32 @@
 package com.example.imgeditor
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.imgeditor.databinding.ActivityImgEditingBinding
+
 
 class ImgEditing : AppCompatActivity() {
 
     private lateinit var binding:ActivityImgEditingBinding
+    var bitmap: Bundle? =null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityImgEditingBinding.inflate(layoutInflater)
         setContentView(binding.imgediting)
+
+        if (intent.hasExtra("byteArray")) {
+            val _imv = binding.imageView
+            val _bitmap = BitmapFactory.decodeByteArray(
+                intent.getByteArrayExtra("byteArray"),
+                0,
+                intent.getByteArrayExtra("byteArray")!!.size
+            )
+            _imv.setImageBitmap(_bitmap)
+        }
+
     }
 }
--- app/src/main/java/com/example/imgeditor/MainActivity.kt
+++ app/src/main/java/com/example/imgeditor/MainActivity.kt
@@ -1,24 +1,24 @@
 package com.example.imgeditor
 
+
 import android.Manifest
-import android.app.Activity
 import android.content.ActivityNotFoundException
-import android.content.ContentValues
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.imgeditor.databinding.ActivityMainBinding
+import java.io.ByteArrayOutputStream
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     val REQUEST_IMAGE_CAPTURE = 1
+    val REQUEST_GALLERY=2
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,9 +30,14 @@ class MainActivity : AppCompatActivity() {
             dispatchTakePictureIntent();
         }
         binding.BtnGallery.setOnClickListener{
-
+            dispatachPickPhotoIntent();
         }
 
+    }
+    private fun dispatachPickPhotoIntent() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_GALLERY)
     }
     private fun dispatchTakePictureIntent() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
@@ -40,6 +45,24 @@ class MainActivity : AppCompatActivity() {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
         } catch (e: ActivityNotFoundException) {
             // display error state to the user
+        }
+    }
+
+
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK ||
+            requestCode == REQUEST_GALLERY
+                ) {
+            val _intent = Intent(this, ImgEditing::class.java)
+            var _bitmap: Bitmap = data?.extras?.get("data") as Bitmap
+
+            val _bs = ByteArrayOutputStream()
+            _bitmap.compress(Bitmap.CompressFormat.PNG, 50, _bs)
+            _intent.putExtra("byteArray", _bs.toByteArray())
+            startActivity(_intent)
         }
     }
 
