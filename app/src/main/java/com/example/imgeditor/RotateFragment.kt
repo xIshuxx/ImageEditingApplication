a81dab8322fa7fc079bb6ae618cff35bf9bfcc46
Ishaan <ishaanvash@gmail.com>
2022-05-05 23:47:43 +0700

    Rotation Done.

--- app/src/main/java/com/example/imgeditor/RotateFragment.kt
+++ app/src/main/java/com/example/imgeditor/RotateFragment.kt
@@ -1,24 +1,31 @@
 package com.example.imgeditor
 
-import android.app.Activity
-import android.content.Context
-import android.content.ContextWrapper
+import android.graphics.Bitmap
+import android.graphics.Matrix
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
+import androidx.core.view.drawToBitmap
 import androidx.fragment.app.Fragment
 import com.example.imgeditor.databinding.FragmentRotateBinding
 
 
 class RotateFragment(imgEditing: ImgEditing) :Fragment(){
-    lateinit var binding: FragmentRotateBinding
-    var activity=imgEditing
+
+
+    private lateinit var binding: FragmentRotateBinding
+    private lateinit var org:Bitmap
+    private var activity=imgEditing
+    private var counter:Float=0f
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        org= activity.binding.imageView.drawToBitmap()
     }
 
     override fun onCreateView(
@@ -29,10 +36,32 @@ class RotateFragment(imgEditing: ImgEditing) :Fragment(){
         val view= inflater.inflate(R.layout.fragment_rotate,container,false)
         val btn= view?.findViewById<Button>(R.id.BtnRotate)
         btn?.setOnClickListener {
-            activity.binding.imageView.setImageBitmap(null)
+
+            activity.binding.imageView.setImageBitmap(rotateImage90deg())
         }
         return view
     }
+
+    private fun rotateImage90deg():Bitmap{
+
+        var rotated:Bitmap
+        counter +=90
+        if(counter==360f){
+            counter=0f
+        }
+        val matrix = Matrix()
+        matrix.postRotate(counter)
+        rotated = Bitmap.createBitmap(
+            org, 0, 0,
+            org.getWidth(), org.getHeight(),
+            matrix, true
+        )
+
+        return rotated
+    }
+
+
+
 
 
 }
